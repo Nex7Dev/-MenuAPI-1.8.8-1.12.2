eeec820bbb008fae093287aa8aaac427c12ab498
Nex7Dev <vmoyarodrigue@gmail.com>
2026-02-11 02:45:32 -0300

    Fix PaginatedMenu.java

--- src/main/java/me/nex7dev/menuapi/system/PaginatedMenu.java
+++ src/main/java/me/nex7dev/menuapi/system/PaginatedMenu.java
@@ -1,7 +1,10 @@
 package me.nex7dev.menuapi.system;
 
+import me.nex7dev.menuapi.utils.ItemBuilder;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public abstract class PaginatedMenu extends Menu {
 
@@ -9,44 +12,59 @@ public abstract class PaginatedMenu extends Menu {
     protected int maxItemsPerPage = 28;
     protected int index = 0;
 
-    public PaginatedMenu(PlayerMenuUtility playerMenuUtility) {
-        super(playerMenuUtility);
+    public PaginatedMenu(String title) {
+        super(title, 54);
     }
 
     public void addMenuBorder() {
-        inventory.setItem(48, makeItem(Material.ARROW, ChatColor.GREEN + "Left"));
-        inventory.setItem(49, makeItem(Material.BARRIER, ChatColor.DARK_RED + "Close"));
-        inventory.setItem(50, makeItem(Material.ARROW, ChatColor.GREEN + "Right"));
+        ItemStack filler = new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (short) 15).setName(" ").build();
+        Button fillerButton = new Button(filler);
+
+        setButton(48, new Button(new ItemBuilder(Material.ARROW).setName(ChatColor.GREEN + "Previous").build(), event -> {
+            if (page == 0) {
+                ((Player) event.getWhoClicked()).sendMessage(ChatColor.RED + "You are already on the first page.");
+            } else {
+                page = page - 1;
+                super.open((Player) event.getWhoClicked());
+            }
+        }));
+
+        setButton(49, new Button(new ItemBuilder(Material.BARRIER).setName(ChatColor.DARK_RED + "Close").build(), event -> {
+            event.getWhoClicked().closeInventory();
+        }));
+
+        setButton(50, new Button(new ItemBuilder(Material.ARROW).setName(ChatColor.GREEN + "Next").build(), event -> {
+            if (!((index + 1) >= getDataSize())) {
+                page = page + 1;
+                super.open((Player) event.getWhoClicked());
+            } else {
+                ((Player) event.getWhoClicked()).sendMessage(ChatColor.RED + "You are on the last page.");
+            }
+        }));
 
         for (int i = 0; i < 10; i++) {
             if (inventory.getItem(i) == null) {
-                inventory.setItem(i, super.FILLER_GLASS);
+                setButton(i, fillerButton);
             }
         }
 
-        inventory.setItem(17, super.FILLER_GLASS);
-        inventory.setItem(18, super.FILLER_GLASS);
-        inventory.setItem(26, super.FILLER_GLASS);
-        inventory.setItem(27, super.FILLER_GLASS);
-        inventory.setItem(35, super.FILLER_GLASS);
-        inventory.setItem(36, super.FILLER_GLASS);
+        setButton(17, fillerButton);
+        setButton(18, fillerButton);
+        setButton(26, fillerButton);
+        setButton(27, fillerButton);
+        setButton(35, fillerButton);
+        setButton(36, fillerButton);
 
         for (int i = 44; i < 54; i++) {
             if (inventory.getItem(i) == null) {
-                inventory.setItem(i, super.FILLER_GLASS);
+                setButton(i, fillerButton);
             }
         }
     }
 
+    public abstract int getDataSize();
+
     public int getMaxItemsPerPage() {
         return maxItemsPerPage;
-    }
-    
-    private ItemStack makeItem(Material material, String name) {
-        org.bukkit.inventory.ItemStack item = new org.bukkit.inventory.ItemStack(material);
-        org.bukkit.inventory.meta.ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(name);
-        item.setItemMeta(meta);
-        return item;
     }
 }
